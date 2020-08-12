f4070339178e080f272a44c14908575e89f9610d
jhin93 <scene1993@gmail.com>
2020-08-12 15:36:23 +0900

    8/12 해쉬를 이용한 문제풀이

--- Leetcode/Array/Intersection of Two Arrays 2.js
+++ Leetcode/Array/Intersection of Two Arrays 2.js
@@ -4,17 +4,16 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    //해쉬
+    //해쉬를 이용해서 풀 수 있음. 먼저 해쉬객체와 배열을 생성.
     const hash = {}, ar = []
-    //nums1 = [1,2,2,1] nums2 = [2,2]
-    //nums2의 요소가 hash라는 객체에 존재하면 그 프로퍼티를 ++, 없으면 1 대입.
+    //예시 파라미터 nums1 = [1,2,2,1] nums2 = [2,2]
+    //nums2의 요소가 hash라는 객체에 존재하면 그 프로퍼티를 1씩 증가시키고, 없으면 1 대입.
     nums2.forEach(num => {
         hash[num] ? hash[num]++ : hash[num] = 1
-        console.log(hash)
     })
     // hash === { '2' : 2 }
     for (let i = 0; i < nums1.length; i++){
-        //만약 hash에 nums1의 요소가 존재한다면
+        //만약 hash에 nums1의 요소가 존재한다면 최소 1일 것이기에 조건으로 담.
         if (hash[nums1[i]] > 0){
             //그 요소를 ar에 삽입,
             ar.push(nums1[i])
