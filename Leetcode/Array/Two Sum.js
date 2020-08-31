81d55a7327dfe908755d1df62840114d6b1929b3
jhin93 <scene1993@gmail.com>
2020-08-31 23:06:08 +0900

    8/31 알고리즘 풀이

---
+++ Leetcode/Array/Two Sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i=0;i<nums.length;i++){
+        //내가 해결하지 못했던 요소 제외를 i+1로 간단히 해결할 수 있었다.
+        for(let j=i+1;j<nums.length;j++){
+            if((nums[i] + nums[j]) === target)
+                return [i, j];
+        }
+    }
+  };
