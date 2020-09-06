39c12f1ffdae5ff9117314278833d03e4b26ac24
jhin93 <scene1993@gmail.com>
2020-09-06 17:54:29 +0900

    9/6 알고리즘 좀 더 쉬운 이해

--- Leetcode/Strings/Valid Anagram.js
+++ Leetcode/Strings/Valid Anagram.js
@@ -1,7 +1,16 @@
     //해쉬를 이용한 풀이이다. 
     //s는 객체에 키와 밸류값을 증가시키고 t는 감소시킨다.
     //만약 두 문자가 같다면 증가한 만큼 감소할 테니 모든 요소가 0 이냐는 함수를 리턴했을 때 true 혹은 false가 도출된다.
-	const map = {};
-	s.split('').map(c => map[c] = map[c] ? map[c] + 1 : 1);
-	t.split('').map(c => map[c] = map[c] ? map[c] - 1 : -1);
-	return Object.keys(map).every(k => map[k] === 0);
+    if (s.length !== t.length) return false;
+    const map = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] ? map[s[i]]++ : map[s[i]] = 1;
+    }
+    
+    for (let i = 0; i < t.length; i++) {
+        if (map[t[i]]) map[t[i]]--;
+        else return false;    
+    }
+
+    return true;
